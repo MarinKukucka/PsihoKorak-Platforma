a54295a7c10423f8ecbbfe075493e8f4e99609c8
MarinKukucka <mk53435@fer.hr>
2024-05-25 22:49:42 +0200

    Add tests

---
+++ PsihoKorak Platforma/PsihoKorak Platforma.Tests/Utils/CryptoUtilsTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using PsihoKorak_Platforma.Utils;
+
+namespace PsihoKorak_Platforma.Tests.Utils
+{
+    public class CryptoUtilsTests
+    {
+        [Fact]
+        public void GetSalt_ReturnsNonEmptyArray()
+        {
+            var salt = CryptoUtils.GetSalt();
+
+            salt.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public void HashPassword_GeneratesValidHash()
+        {
+            var password = "password123";
+            var salt = CryptoUtils.GetSalt();
+
+            var hashedPassword = CryptoUtils.HashPassword(password, salt);
+
+            hashedPassword.Should().NotBeNullOrEmpty();
+            hashedPassword.Length.Should().Be(44);
+        }
+
+        [Fact]
+        public void HashPassword_ReturnsDifferentHashForDifferentSalt()
+        {
+            var password = "password123";
+            var salt1 = CryptoUtils.GetSalt();
+            var salt2 = CryptoUtils.GetSalt();
+
+            var hashedPassword1 = CryptoUtils.HashPassword(password, salt1);
+            var hashedPassword2 = CryptoUtils.HashPassword(password, salt2);
+
+            hashedPassword1.Should().NotBe(hashedPassword2);
+        }
+    }
+}
